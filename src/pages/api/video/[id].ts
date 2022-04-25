ab99d6b4829968b1c3b9b87b31cdc2659bbe7933
Dusty McCord <Dustymccord@gmail.com>
2022-04-24 23:03:59 -0700

    Add api endpoint for edit

--- src/pages/api/video/[id].ts
+++ src/pages/api/video/[id].ts
@@ -1,6 +1,6 @@
 import prisma from "../../../lib/prisma"
 
-// DELETE /api/post/:id
+// DELETE /api/video/:id
 export default async function handle(req, res) {
   const postId = req.query.id
   if (req.method === "DELETE") {
@@ -8,6 +8,19 @@ export default async function handle(req, res) {
       where: { id: postId },
     })
     res.json(post)
+  } else if (req.method === "POST") {
+    const { title, content, image, link } = req.body
+    const videoId = req.query.id
+    const video = await prisma.video.update({
+      where: { id: videoId },
+      data: {
+        title: title,
+        content: content,
+        image: image,
+        link: link,
+      },
+    })
+    res.json(video)
   } else {
     throw new Error(
       `The HTTP ${req.method} method is not supported at this route.`
